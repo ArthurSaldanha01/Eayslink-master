11b7e81426e131606ce55fe692eecc343579fb1d
ArthurSaldanha01 <arthursaldanha101@gmail.com>
2022-03-14 19:22:20 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
+
+
+function mudarValor(func, cont) {
+    document.getElementById("conteudo" + String(cont)).innerHTML = func;
+}
+
+function devolverValor(nome, cont) {
+    document.getElementById("conteudo" + String(cont)).innerHTML = nome;
+}
